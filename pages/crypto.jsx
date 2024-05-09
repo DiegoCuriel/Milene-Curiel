a40d3d20e581b6e7426ddc881680c2f2acf9e748
DiegoCuriel <a01640372@itesm.mx>
2024-05-08 22:02:09 -0600

    typo collaborated on

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -16,7 +16,7 @@ const Crypto = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Collaborated</h2>
+          <h2 className="py-2">Collaborated on</h2>
         </div>
       </div>
 
